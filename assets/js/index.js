46dc3292fbd3ddb18c235020d76ebe8e8374e7fe
Suliman Khan <sulimank418@gmail.com>
2022-09-20 10:54:54 +0500

    Allahamdulliah Completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,8 +55,34 @@ $('.annoucement-slider .row').owlCarousel({
     loop: true,
     dots: false,
     nav: true,
-    navContainer:$('.slider-nav'),
+    navContainer: $('.slider-nav'),
     navText: ['<img src="../assets/img/prev.svg" width="10">', '<img src="../assets/img/next.svg" width="10">'],
+    responsive: {
+        0: {
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplaySpeed: 1000,
+        },
+        768: {
+            items: 2,
+        },
+        1024: {
+            items: 3,
+        }
+    }
+})
+$('#slider').owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    dotsContainer :$('.dots-container'),
+    nav: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1000,
+    dragEndSpeed: 500,
 })
 
 
