a515c344059fdc77133483e0f10d24a16e5574d8
Suliman Khan <sulimank418@gmail.com>
2022-09-27 11:34:00 +0500

    Mega Menu Integrated

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,8 +12,44 @@
         }, false)
     })
 })()
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth > 992) {
 
-// Init reviw
+        document.querySelectorAll('.navbar .nav-item').forEach(function (item) {
+
+            item.addEventListener('mouseover', function (e) {
+
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if (el_link != null) {
+                    el_link.classList.add('show')
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+                
+            });
+            item.addEventListener('mouseleave', function (e) {
+                let el_link = this.querySelector('a[data-bs-toggle]');
+                
+                if (el_link != null) {
+                    el_link.classList.remove('show')
+                    let nextEl = el_link.nextElementSibling;
+                    setTimeout(()=>{
+                        el_link.classList.remove('show');
+                        nextEl.classList.remove('show');
+                    },500)
+                }
+
+
+            })
+        });
+
+    }
+    // end if innerWidth
+});
+
+// Init review
 $('.reviews-slider').owlCarousel({
     stagePadding: 150,
     autoplay: true,
